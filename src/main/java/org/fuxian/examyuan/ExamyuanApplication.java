efb951361c9b600f11b27a6b6caff037fd1ffa32
fusxian <870086043@qq.com>
2020-02-18 23:03:44 +0800

    first init project

---
+++ src/main/java/org/fuxian/examyuan/ExamyuanApplication.java
@@ -0,0 +1,13 @@
+package org.fuxian.examyuan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamyuanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExamyuanApplication.class, args);
+    }
+
+}
